7822437f683d89bdeb1fff7ad8d23d6e25eb803d
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-06-11 16:45:11 -0400

    Added the Reset Password System Email Message Template

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,43 +1,78 @@
 import com.cogda.common.MarkupLanguage
+import com.cogda.common.RegistrationStatus
 import com.cogda.domain.admin.CompanyType
 import com.cogda.domain.admin.SystemEmailMessageTemplate
+import com.cogda.domain.onboarding.Registration
 import com.cogda.multitenant.Company
 import com.cogda.multitenant.CustomerAccount
 import com.cogda.domain.admin.SupportedCountryCode
+import com.cogda.multitenant.CustomerAccountService
+import grails.plugins.springsecurity.SpringSecurityService
 import grails.util.Environment
 import grails.util.GrailsUtil
 
 class BootStrap {
+    CustomerAccountService customerAccountService
+    SpringSecurityService springSecurityService
 
     def init = { servletContext ->
 
+
+
         if(Environment.current != Environment.TEST){
-            CompanyType agency = new CompanyType(code:"Agency/Retailer", intCode:0, description: "Agency/Retailer").save()
-            CompanyType carrier = new CompanyType(code:"Carrier", intCode:1, description: "Carrier").save()
-            CompanyType reinsurer = new CompanyType(code:"Reinsurer", intCode:2, description: "Reinsurer").save()
-            CompanyType wholesaler = new CompanyType(code:"Wholesaler (MGA, Broker)", intCode:3, description: "Wholesaler (MGA, Broker)").save()
 
-            CustomerAccount rpsCustomerAccount = CustomerAccount.findOrSaveBySubDomain("rps")
-            CustomerAccount raisCustomerAccount = CustomerAccount.findOrSaveBySubDomain("renaissanceins")
-
-            raisCustomerAccount.withThisTenant {
-                Company company = new Company()
-                company.companyName = "Renaissance Alliance"
-                company.doingBusinessAs = "Renaissance Alliance"
-                company.intCode = 0
-
-                if(!company.save()){
-                    company.errors.each {
-                        log.debug(it)
-                    }
-                }
+            if(!CompanyType.findByCode("Agency/Retailer")){
+                new CompanyType(code:"Agency/Retailer", intCode:0, description: "Agency/Retailer").save()
+            }
+            if(!CompanyType.findByCode("Carrier")){
+                new CompanyType(code:"Carrier", intCode:1, description: "Carrier").save()
+            }
+            if(!CompanyType.findByCode("Reinsurer")){
+                new CompanyType(code:"Reinsurer", intCode:2, description: "Reinsurer").save()
+            }
+            if(!CompanyType.findByCode("Wholesaler (MGA, Broker)")){
+                new CompanyType(code:"Wholesaler (MGA, Broker)", intCode:3, description: "Wholesaler (MGA, Broker)").save()
             }
 
 
+            if(!SupportedCountryCode.findByCountryCode("usa")){
+                new SupportedCountryCode(countryCode:"usa", countryDescription:"United States").save()
+            }
+            if(!SupportedCountryCode.findByCountryCode("usa")){
+                new SupportedCountryCode(countryCode:"can", countryDescription:"Canada").save()
+            }
+//        SupportedCountryCode bra = new SupportedCountryCode(countryCode:"bra", countryDescription:"Brazil").save()
 
-            SupportedCountryCode usa = new SupportedCountryCode(countryCode:"usa", countryDescription:"United States").save()
-            SupportedCountryCode can = new SupportedCountryCode(countryCode:"can", countryDescription:"Canada").save()
-            //        SupportedCountryCode bra = new SupportedCountryCode(countryCode:"bra", countryDescription:"Brazil").save()
+            if(!Registration.findBySubDomain("rais")){
+                Registration registration
+                Registration.withTransaction {
+                    registration = new Registration()
+
+                    registration.firstName = "Maria"
+                    registration.lastName = "Schiller"
+                    registration.username = "admin"
+                    registration.emailAddress = "chris@cogda.com"
+                    registration.password = springSecurityService.encodePassword("password")
+                    registration.companyName = "Renaissance Alliance"
+                    registration.companyType = CompanyType.findByCode("Wholesaler (MGA, Broker)")
+                    registration.existingCompany = null
+                    registration.companyTypeOther = null
+                    registration.phoneNumber = "706-255-9087"
+                    registration.streetAddressOne = "1 Press Place"
+                    registration.streetAddressTwo = "Suite 200"
+                    registration.streetAddressThree = "Office #17"
+                    registration.city = "Athens"
+                    registration.state = "GA"
+                    registration.zipcode = "30601"
+                    registration.county = "CLARKE"
+                    registration.registrationStatus = RegistrationStatus.APPROVED
+                    registration.subDomain = "rais"
+
+                    assert registration.save(), "Registration save failed: ${registration.errors}"
+                }
+
+                customerAccountService.onboardCustomerAccount(registration)
+            }
 
 
             if(!SystemEmailMessageTemplate.findByTitle("INITIAL_ACCOUNT_ACTIVATION_EMAIL")){
