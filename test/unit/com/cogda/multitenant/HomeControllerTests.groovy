b34f491791581dd230b0b20b619bbf84218a5bcd
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-06-14 10:36:57 -0400

    Unit tests added

--- test/unit/com/cogda/multitenant/HomeControllerTests.groovy
+++ test/unit/com/cogda/multitenant/HomeControllerTests.groovy
@@ -3,15 +3,33 @@ package com.cogda.multitenant
 
 
 import grails.test.mixin.*
+import grails.test.mixin.domain.DomainClassUnitTestMixin
 import org.junit.*
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(HomeController)
+@TestMixin(DomainClassUnitTestMixin)
 class HomeControllerTests {
 
-    void testSomething() {
-        fail "Implement me"
+    void testIndex() {
+
+        mockDomain(Company)
+        mockDomain(CustomerAccount, [new CustomerAccount(subDomain: "newAccount", id:1)])
+        mockDomain(Company, [new Company(parentCompany: null, companyName:"testcompany", doingBusinessAs:"testcompany", intCode:0, )])
+
+        request.method = "GET"
+        request.putAt("customerAccount", 1)
+
+        controller.index()
+
+        assert view == "/home/index"
+        assert model.companyInstance
+        assert model.customerAccountInstance
+
+
+
+
     }
 }
