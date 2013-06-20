876552931498b1340d31b557b91cc9683f4eedc9
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-06-20 11:20:07 -0400

    added convenience methods to the Account domain class

--- grails-app/domain/com/cogda/multitenant/Account.groovy
+++ grails-app/domain/com/cogda/multitenant/Account.groovy
@@ -20,11 +20,76 @@ class Account {
      */
     AccountType accountType
 
+    /* Automatic timestamping of GORM */
+    Date	dateCreated
+    Date	lastUpdated
+
     static hasMany = [accountContacts:AccountContact]
+
+    static transients = ["primaryEmailAddress", "primaryAccountEmailAddress", "primaryAccountContact", "primaryAccountContactName"]
 
     static constraints = {
         accountName(nullable:false, blank:false, unique:"tenantId")
         accountCode(nullable:true)
 
+    }
+
+    /**
+     * Get the Primary Email Address for this Account
+     * @return String
+     */
+    public String getPrimaryEmailAddress(){
+        return getPrimaryAccountEmailAddress()?.emailAddress
+    }
+
+    /**
+     * Retrieves the primary account's primary AccountEmailAddress
+     * @return AccountEmailAddress
+     */
+    public AccountEmailAddress getPrimaryAccountEmailAddress(){
+        AccountEmailAddress accountEmailAddress = this.getPrimaryAccountContact()?.accountEmailAddresses?.find {
+            it.primaryEmailAddress == Boolean.TRUE
+        }
+        return accountEmailAddress
+    }
+
+    /**
+     * Retrieves the primary account contact's name.
+     * @return String
+     */
+    public String getPrimaryAccountContactName(){
+        return getPrimaryAccountContact()?.fullName
+    }
+
+    /**
+     * Retrieves the AccountContact marked as primaryAccount
+     * @return AccountContact
+     */
+    public AccountContact getPrimaryAccountContact(){
+        AccountContact accountContact = this.accountContacts?.find {
+            it.primaryContact == Boolean.TRUE
+        }
+
+        return accountContact
+    }
+
+    /**
+     * Retrieves the primary account's primary AccountPhoneNumber
+     * @return AccountPhoneNumber
+     */
+    public AccountPhoneNumber getPrimaryAccountContactPhoneNumber(){
+        AccountPhoneNumber accountPhoneNumber = getPrimaryAccountContact()?.accountPhoneNumbers?.find {
+            it.primaryPhoneNumber == Boolean.TRUE
+        }
+
+        return accountPhoneNumber
+    }
+
+    /**
+     * Retrieves the primary account's primary AccountPhoneNumber.phoneNumber
+     * @return String
+     */
+    public String getPrimaryAccountContactPhoneNumberString(){
+        return getPrimaryAccountContactPhoneNumber()?.phoneNumber
     }
 }
--- grails-app/domain/com/cogda/multitenant/AccountContact.groovy
+++ grails-app/domain/com/cogda/multitenant/AccountContact.groovy
@@ -23,23 +23,30 @@ class AccountContact {
     UserProfile userProfile
 
     /**
-     *
+     * First name
      */
     String firstName
 
     /**
-     *
+     * Middle name
      */
     String middleName
 
     /**
-     *
+     * Last name
      */
     String lastName
+
+    /**
+     * Primary contact
+     */
+    Boolean primaryContact
 
     static hasMany = [accountAddresses:AccountAddress,
             accountEmailAddresses:AccountEmailAddress,
             accountPhoneNumbers:AccountPhoneNumber]
+
+    static transients = ["fullName"]
 
     /* Automatic timestamping of GORM */
 	Date	dateCreated
@@ -48,11 +55,18 @@ class AccountContact {
 	static belongsTo	= [account:Account]	// tells GORM to cascade commands: e.g., delete this object if the "parent" is deleted.
 
 
-    static mapping = {
-
+    static constraints = {
+        firstName(nullable:false, blank:false)
+        middleName(nullable:true)
+        lastName(nullable:false, blank:false)
+        primaryContact(nullable:true)
     }
 
-    static constraints = {
-
+    /**
+     * Gets the full name - lastName, firstName
+     * @return String
+     */
+    String getFullName(){
+        return "${lastName}, ${firstName}"
     }
 }
