198ffa1df813e4a0aad5747da1094c8e5432011c
angelkbrown <angelkbrown@gmail.com>
2013-06-20 15:43:14 -0400

    adding ability to add additional email, address and phone fields to the modal ui

--- web-app/js/contact/contact.js
+++ web-app/js/contact/contact.js
@@ -6,14 +6,44 @@ function toggleEdit(){
 }
 function saveContact(){
   toggleEdit();
-  // save
+  var contactDeets = new Object();
+  var contact = new Object();
+  contactDeets.contact = contact;
+  contact.id = $("form").attr("id").replace("contactForm_","");
+  contact.firstName = $("#firstName").val();
+  contact.lastName = $("#lastName").val();  
+  $.post("/contact/update", JSON.stringify(contactDeets), function(){alert('success!');});
+}
+
+function addEmailAddressField(){
+  var count = $("#emailFieldset div.field").length;
+  var field = $(document.createElement("div")).addClass("field");
+  $(field).append($(document.createElement("label")).append("Email Address"));
+  $(field).append($(document.createElement("input")).attr("id","emailAddress_"+count).val("").attr("type","text"));  
+	$("#emailFieldset").append($(field));
+}
+
+function addMailingAddressField(){
+  var count = $("#mailFieldset div.field").length;  
+  var field = $(document.createElement("div")).addClass("field");
+  $(field).append($(document.createElement("label")).append("Mailing Address"));
+  $(field).append($(document.createElement("input")).attr("id","mailingAddress_"+count).val("").attr("type","text"));  
+	$("#mailFieldset").append($(field));
+}
+
+function addPhoneField(){
+  var count = $("#phoneFieldset div.field").length;  
+  var field = $(document.createElement("div")).addClass("field");
+  $(field).append($(document.createElement("label")).append("Phone"));
+  $(field).append($(document.createElement("input")).attr("id","phone_"+count).val("").attr("type","text"));  
+	$("#phoneFieldset").append($(field));
 }
 
 $(document).ready(function() {
     $('#contactList').dataTable(
     {
         "bProcessing": true,
-        "sAjaxSource": "contact/list",
+        "sAjaxSource": "/contact/list",
         "aoColumns": [
             {"mDataProp":"companyName"},
             {"mDataProp":"lastName"},
@@ -29,7 +59,8 @@ $(document).ready(function() {
                 $(this).toggleClass('row_selected');
             });
             $(nRow).dblclick( function() {
-                $.get("contact/get/"+$(this).attr("id").replace("row_",""), function(data) {
+                var rowId = $(this).attr("id").replace("row_","");
+                $.get("/contact/get/"+rowId, function(data) {
                     $("#firstName").val(data.modelObject.firstName);
                     $(".firstName").text(data.modelObject.firstName);                    
                     $("#lastName").val(data.modelObject.lastName);
@@ -37,7 +68,8 @@ $(document).ready(function() {
                     $("#companyName").val(data.modelObject.companyName);
                     $(".companyName").text(data.modelObject.companyName);                    
                     $("#jobTitle").val(data.modelObject.jobTitle);                    
-                    $(".jobTitle").text(data.modelObject.jobTitle);                                        
+                    $(".jobTitle").text(data.modelObject.jobTitle);  
+                    $('#contactForm').attr("id","contactForm_"+rowId);                       
                     $('#contactModal').modal('show');
                 });
             });
