d131f7fa6bf55a06b36e9491da87f14edc7ee055
Kenneth Lee Pilgrim <lpilgrim@cogda.com>
2013-06-21 08:41:42 -0400

    #17 - Closed - Create Unit Tests for the AccountAddress Domain Class

---
+++ test/unit/com/cogda/domain/AccountAddressTests.groovy
@@ -0,0 +1,44 @@
+package com.cogda.domain
+
+import com.cogda.multitenant.AccountAddress
+import grails.test.mixin.*
+import grails.test.mixin.domain.DomainClassUnitTestMixin
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(AccountAddress)
+@TestMixin(DomainClassUnitTestMixin)
+class AccountAddressTests {
+
+    AccountAddress accountAddress
+
+    void setUp() {
+        // Setup logic here
+    }
+
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    /**
+     * class AccountAddress {
+     * 	...
+     * 	accountAddressName(nullable:false, blank:false)
+     * 	accountAddressCode(nullable:true)
+     *  ...
+     */
+    void testNullable(){
+        mockDomain(AccountAddress)
+
+        accountAddress = new AccountAddress()
+
+        assertFalse accountAddress.validate()
+        //accountAddress.errors.each { println it }
+
+        assert "nullable" == accountAddress.errors["address"].code
+        assert "nullable" == accountAddress.errors["account"].code
+        assert !accountAddress.errors["primaryAddress"]
+
+    }
+}
