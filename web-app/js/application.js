672422a6a12c9ae6c3d9e12d60ada4c57a5d7c07
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-06-30 01:06:50 -0400

    cogda-49 Complete the User Upload from CSV File and Corresponding Tests

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -1,9 +1,31 @@
-    if (typeof jQuery !== 'undefined') {
-        (function($) {
-            $('#spinner').ajaxStart(function() {
-                $(this).fadeIn();
-            }).ajaxStop(function() {
-                $(this).fadeOut();
-            });
-        })(jQuery);
-    }
+if (typeof jQuery !== 'undefined') {
+    (function($) {
+        $('#spinner').ajaxStart(function() {
+            $(this).fadeIn();
+        }).ajaxStop(function() {
+            $(this).fadeOut();
+        });
+    })(jQuery);
+}
+
+/**
+ * Serialize a form object with this method:
+ * e.g. JSON.stringify($('#pendingUserForm').serializeObject())
+ * @returns {{}}
+ */
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
