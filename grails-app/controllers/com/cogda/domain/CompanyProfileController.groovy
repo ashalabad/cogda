8ec676a4892f9b79e417a5ee6f61753596026a6d
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-07-10 08:09:18 -0400

    860781-53070793 Admin user can add company profile information with their company details.
    Renamed the CompanyProfileController

---
+++ grails-app/controllers/com/cogda/domain/CompanyProfileController.groovy
@@ -0,0 +1,97 @@
+package com.cogda.domain
+
+import com.cogda.GsonBaseController
+import grails.plugin.gson.converters.GSON
+import org.springframework.dao.DataIntegrityViolationException
+import static grails.plugin.gson.http.HttpConstants.*
+
+/**
+ * CompanyProfileController
+ * A controller class handles incoming web requests and performs actions such as redirects, rendering views and so on.
+ */
+class CompanyProfileController extends GsonBaseController{
+
+    final static String COMPANY_PROFILE_LABEL = "companyProfile.label"
+
+    def index(){
+
+    }
+
+    def showPartial(){
+        render (view:'showPartial')
+    }
+
+    def list(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        response.addIntHeader X_PAGINATION_TOTAL, CompanyProfile.count()
+        render CompanyProfile.list(params) as GSON
+    }
+
+    def save() {
+        if (!requestIsJson()) {
+            respondNotAcceptable()
+            return
+        }
+
+        def companyProfileInstance = new CompanyProfile(request.GSON)
+        if (companyProfileInstance.save(flush: true)) {
+            respondCreated companyProfileInstance
+        } else {
+            respondUnprocessableEntity companyProfileInstance
+        }
+    }
+
+    def get() {
+        CompanyProfile companyProfileInstance = CompanyProfile.rootCompanyProfile()
+        if (companyProfileInstance) {
+            respondFound companyProfileInstance
+        } else {
+            respondNotFound COMPANY_PROFILE_LABEL
+        }
+    }
+
+    def update() {
+        if (!requestIsJson()) {
+            respondNotAcceptable()
+            return
+        }
+
+        def companyProfileInstance = CompanyProfile.get(params.id)
+        if (!companyProfileInstance) {
+            respondNotFound COMPANY_PROFILE_LABEL
+            return
+        }
+
+        if (params.version != null) {
+            if (companyProfileInstance.version > params.long('version')) {
+                respondConflict(companyProfileInstance)
+                return
+            }
+        }
+
+        companyProfileInstance.properties = request.GSON
+
+        if (companyProfileInstance.save(flush: true)) {
+            respondUpdated companyProfileInstance
+        } else {
+            respondUnprocessableEntity companyProfileInstance
+        }
+    }
+
+    def delete() {
+        def companyProfileInstance = CompanyProfile.get(params.id)
+        if (!companyProfileInstance) {
+            respondNotFound COMPANY_PROFILE_LABEL
+            return
+        }
+
+        try {
+            companyProfileInstance.delete(flush: true)
+            respondDeleted()
+        } catch (DataIntegrityViolationException e) {
+            respondNotDeleted COMPANY_PROFILE_LABEL
+        }
+    }
+
+
+}
