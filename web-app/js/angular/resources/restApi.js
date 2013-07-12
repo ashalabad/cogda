4d7de26999ffbe978ec37578a35b21706c8340cf
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-07-12 13:39:30 -0400

    860781-53070793 Admin user can add company profile information with their company details.
    #860781-53070793 restApi change

--- web-app/js/angular/resources/restApi.js
+++ web-app/js/angular/resources/restApi.js
@@ -9,12 +9,13 @@ angular.module('resources.restApi', ['ngResource']).factory('RestApi', function(
     }
 
     function getRest(baseUrl){
-        return $resource(baseUrl + ':action/:id', {id: '@id'}, {
-            list: {method: 'GET', params: {action: 'list'}, isArray: true},
-            get: {method: 'GET', params: {action: 'get'}},
-            save: {method: 'POST', params: {action: 'save'}},
-            update: {method: 'POST', params: {action: 'update'}},
-            delete: {method: 'POST', params: {action: 'delete'}}
+        return $resource(baseUrl + ':action/:id', {id: '@id'},   {
+            list:     {method: 'GET',  params:  {action: 'list'}, isArray: true},     // list objects
+            search:   {method: 'GET',  params:  {action: 'search'}, isArray: true},   // search objects with ?q=mysearchterm
+            get:      {method: 'GET',  params:  {action: 'get'}    },                 // get one object
+            save:     {method: 'POST', params:  {action: 'save'}   },                 // create a new object
+            update:   {method: 'POST', params:  {action: 'update'} },                 // update an object
+            delete:   {method: 'POST', params:  {action: 'delete'} }                  // delete an object
         });
     }
 
