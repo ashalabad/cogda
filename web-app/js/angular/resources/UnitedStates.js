c04d579446d682bfc1209f28963341a57f076ff8
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-07-13 09:58:42 -0400

    860781-53070793 Admin user can add company profile information with their company details.
    #860781-53070793 Added the UnitedStates resource and renamed to capitalized UnitedStates to match our resources convention.

--- web-app/js/angular/resources/UnitedStates.js
+++ web-app/js/angular/resources/UnitedStates.js
@@ -4,7 +4,7 @@
 angular.module('resources.unitedStates', ['ngResource']);
 angular.module('resources.unitedStates', []).factory('UnitedStates', function($resource) {
 
-    var unitedStates = $resource('/unitedStates/:action/:id', {id:'@id'},   {
+    var unitedStates = $resource('/unitedStates/:action/:id', {id:'@id'},   {     // the id in this case is the state abbreviation e.g. GA, FL, etc.
         list:     {method: 'GET',  params:  {action: 'list'}, isArray: true},     // list objects
         search:   {method: 'GET',  params:  {action: 'search'}, isArray: true},   // search objects with ?q=mysearchterm
         get:      {method: 'GET',  params:  {action: 'get'}    }                  // get one object based on the passed in ID
