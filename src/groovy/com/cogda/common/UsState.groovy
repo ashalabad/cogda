fa1b7a4a53098f484b5e4983138e9a34c657494c
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-07-13 12:12:24 -0400

    860781-53070793 Admin user can add company profile information with their company details.
    #860781-53070793 useful convenience methods to be used when a UnitedStates enum needs to be searched or a specific State needs to be found.

--- src/groovy/com/cogda/common/UsState.groovy
+++ src/groovy/com/cogda/common/UsState.groovy
@@ -1,5 +1,7 @@
 package com.cogda.common
 
+import org.apache.commons.lang.StringUtils
+
 /**
  * Created with IntelliJ IDEA.
  * User: christopher
@@ -66,11 +68,119 @@ public enum UsState {
         this.value = value
     }
 
+    /**
+     * Given a UsState usState calling usState.toString() will return
+     * e.g. "WV", "WI", "WY"
+     * @return String
+     */
     String toString() {
         value
     }
 
+    /**
+     * Given a UsState usState calling usState.getKey() will return
+     * e.g. WestVirginia, Wisconsin, Wyoming
+     * @return String
+     */
     String getKey() {
         name()
+    }
+
+    /**
+     * Finds a UsState by the value passed in where the Value
+     * is a state abbreviation WI, WY
+     *
+     * This method ignores case
+     * @param value
+     * @return List
+     */
+    public static List<UsState> findByValue(String value){
+        List<UsState> usStates = []
+        for(UsState usState : values()){
+            if(usState.value.equalsIgnoreCase(value)){
+                usStates.add(usState)
+            }
+        }
+        return usStates;
+    }
+
+    /**
+     * Finds a UsState by the key passed in where the key
+     * is WestVirginia or Wisconsin or Wyoming
+     * This method ignores case
+     * @param key
+     * @return List
+     */
+    public static List<UsState> findByKey(String key){
+        List<UsState> usStates = []
+        for(UsState usState : values()){
+            if(usState.key.equalsIgnoreCase(key)){
+                usStates.add(usState)
+            }
+        }
+        return usStates;
+    }
+
+    /**
+     * Find method for UsState enum
+     * @param q
+     * @return List
+     */
+    public static List<UsState> findByKeyOrValue(String q){
+        List<UsState> usStates = []
+        for(UsState usState : values()){
+            if(StringUtils.containsIgnoreCase(usState.value, q) ||
+                    StringUtils.containsIgnoreCase(usState.key, q)){
+                usStates.add(usState)
+            }
+        }
+        return usStates;
+    }
+    /**
+     * search method for UsState enum
+     * @param q
+     * @return Map
+     */
+    public static Map search(String q){
+        Map<String, String> matches = [:]
+
+        List<UsState> usStates = UsState.findByKeyOrValue(q)
+        usStates.each{ UsState usState ->
+            matches.put(usState.value, usState.key)
+        }
+
+        return matches
+    }
+
+    /**
+     * passing in the value WV
+     * will return UsState.WestVirginia.
+     * passing in a value that is not a UsState will result in a null
+     * @param value
+     * @return UsState or null
+     */
+    public static UsState getByValue(String value){
+        UsState usState = null
+        List usStates = UsState.findByValue(value)
+        if(usStates){
+            usState = usStates.first()
+        }
+        return usState
+    }
+
+    /**
+     * passing in the key Wyoming or wyoMINg
+     * will return UsState.Wyoming.
+     * passing in a key that is not a UsState will result in a null
+     * @param key
+     * @return UsState or null
+     */
+    public static UsState getByKey(String key){
+        UsState usState
+        List usStates = UsState.findByKey(key)
+        if(usStates){
+            usState = usStates.first()
+        }
+        return usState
     }
 }
