2fbba6ad2208da2ea8346e13ee6e94b189057bf0
Kenneth Lee Pilgrim <lpilgrim@cogda.com>
2013-07-19 12:58:33 -0400

    860781-53629371 Build the submission domain classes and the related domain classes.

---
+++ test/unit/com/cogda/multitenant/SubmissionSpec.groovy
@@ -0,0 +1,17 @@
+package com.cogda.multitenant
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Submission)
+class SubmissionSpec {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
