69fcfcb8058e719cbaae1d41f2c153e45d9bf000
Justin Clark <ihcnet@gmail.com>
2013-07-19 12:07:21 -0400

    [#860781-53111975] User should be able to add/edit/delete a Prospect in the Cogda system.
    Added Lead Line Of Business controller and resource. Refactored LeadLineOfBusiness.

--- grails-app/domain/com/cogda/multitenant/LeadLineOfBusiness.groovy
+++ grails-app/domain/com/cogda/multitenant/LeadLineOfBusiness.groovy
@@ -9,13 +9,25 @@ import com.cogda.domain.admin.LineOfBusiness
 class LeadLineOfBusiness {
 
     LineOfBusiness lineOfBusiness
+    Date targetDate
     Date expirationDate
     BigDecimal targetPremium
-    BigDecimal targetCommision
+    BigDecimal targetCommission
     Company writingCompany
     Company billingCompany
     Boolean renewal
     Boolean remarket
 
     static embedded = ['lineOfBusiness']
+
+    static constraints = {
+        targetDate(nullable: true)
+        expirationDate(nullable: true)
+        targetPremium(nullable: true)
+        targetCommission(nullable: true)
+        writingCompany(nullable: true)
+        billingCompany(nullable: true)
+        renewal(nullable: true)
+        remarket(nullable: true)
+    }
 }
