7c32fd477e94fcba2a3684771d9c10dca41dffa2
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-07-19 12:56:16 -0400

    First commit of the SubmissionService

---
+++ grails-app/services/com/cogda/multitenant/SubmissionService.groovy
@@ -0,0 +1,14 @@
+package com.cogda.multitenant
+
+/**
+ * SubmissionService
+ * A service class encapsulates the core business logic of a Grails application
+ */
+class SubmissionService {
+
+    static transactional = true
+
+    def serviceMethod() {
+        // to be continued.
+    }
+}
