b6eaad869586d35240ae1c2cc34edd6a605fd748
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-07-30 07:46:55 -0400

    860781-54242792 Fix the UserProfileEmailAddressControllerTests - they are currently failing.
    added the requestIsJson test

--- test/unit/com/cogda/GsonBaseControllerSpec.groovy
+++ test/unit/com/cogda/GsonBaseControllerSpec.groovy
@@ -1,17 +1,64 @@
 package com.cogda
 
-
-
+import com.google.gson.Gson
 import grails.test.mixin.*
+import grails.test.mixin.web.ControllerUnitTestMixin
 import org.junit.*
+import org.springframework.http.HttpMethod
+import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(GsonBaseController)
-class GsonBaseControllerSpec {
+@TestMixin([ControllerUnitTestMixin])
+class GsonBaseControllerSpec extends Specification {
 
-    void testSomething() {
-        fail "Implement me"
+    Gson gson = new Gson()
+    static final String JSON_CONTENT_TYPE_TEXT = "text/json"
+    static final String JSON_CONTENT_TYPE_APPLICATION = "application/json"
+
+    public String getValidJSON(){
+         return gson.toJson([])
     }
+
+    /**
+     * Returns an invalid JSON string
+     * @return
+     */
+    public String getInvalidJSON(){
+        return gson.toJson(Integer.MAX_VALUE) + "}}}}]]"
+    }
+
+    /**
+     * sets an invalid JSON request on the
+     */
+    void setInvalidJsonRequest(){
+        request.JSON = getInvalidJSON()
+    }
+
+    void setValidJsonRequest(){
+        request.JSON = getValidJSON()
+    }
+
+    @Unroll("test requestIsJson is #valid for #contentType")
+    def "requestIsJson true when application/json contentType"() {
+        when:
+        request.setContentType("$contentType")
+
+        then:
+        valid == controller.requestIsJson()
+
+        where:
+        valid             | contentType
+        false             | 'json'
+        false             | 'text'
+        true              | JSON_CONTENT_TYPE_TEXT
+        true              | JSON_CONTENT_TYPE_APPLICATION
+    }
+
+
+
+
 }
