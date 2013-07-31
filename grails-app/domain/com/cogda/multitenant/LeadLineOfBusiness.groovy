9f5fd90f265401406757df62e864807fafc8e20e
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-07-31 17:44:33 -0400

    860781-54242792 Fix the UserProfileEmailAddressControllerTests - they are currently failing.
    added tests for LeadLineOfBusiness

--- grails-app/domain/com/cogda/multitenant/LeadLineOfBusiness.groovy
+++ grails-app/domain/com/cogda/multitenant/LeadLineOfBusiness.groovy
@@ -8,6 +8,11 @@ import com.cogda.domain.admin.LineOfBusiness
  */
 class LeadLineOfBusiness {
 
+    /**
+     * inject LeadService
+     */
+    LeadService leadService
+
     LineOfBusiness lineOfBusiness
     Date targetDate
     Date expirationDate
@@ -15,17 +20,44 @@ class LeadLineOfBusiness {
     BigDecimal targetCommission
     BigDecimal commissionRate
     String currentCarrier
+    Boolean renewal
     Boolean remarket
 
     static belongsTo = [lead: Lead]
 
     static constraints = {
-        targetDate(nullable: true)
-        expirationDate(nullable: true)
+        targetDate(nullable: true, validator: { Date targetDate, LeadLineOfBusiness leadLineOfBusiness ->
+            if(targetDate > leadLineOfBusiness.expirationDate){
+                return ['leadLineOfBusiness.targetDate.max.exceeded']
+            }
+        })
+        expirationDate(nullable: false, validator: { Date expirationDate, LeadLineOfBusiness leadLineOfBusiness ->
+            Date today = new Date()
+            Date todayPlus180 = today + 180
+            if(expirationDate < (new Date())){
+                return ['leadLineOfBusiness.expirationDate.min.exceeded']
+            }
+            // If there is another expiration date for the same line of business on this lead that is within 180 days time
+            // frame - do not allow them to enter this LeadLineOfBusiness.
+            def leadLinesOfBusinessWithMatchingLobWithinTimeConstraint =
+                leadLineOfBusiness.leadService.findLeadLineOfBusinessesByLobAndExpirationDate(
+                    leadLineOfBusiness.lead.id, leadLineOfBusiness.lineOfBusiness, todayPlus180)
+            if(leadLinesOfBusinessWithMatchingLobWithinTimeConstraint){
+                if(expirationDate < todayPlus180){
+                    return ['leadLineOfBusiness.lineOfBusiness.expirationDate.min.exceeded']
+                }
+            }
+        })
         targetPremium(nullable: true)
-        targetCommission(nullable: true)
+        targetCommission(nullable: true, validator: { BigDecimal targetCommission, LeadLineOfBusiness leadLineOfBusiness ->
+            if(leadLineOfBusiness.targetPremium < targetCommission){
+                return ['leadLineOfBusiness.targetCommission.exceeds.targetPremium']
+            }
+        })
         commissionRate(nullable: true)
         currentCarrier(nullable: true)
         remarket(nullable: true)
+        renewal(nullable:true)
+        lineOfBusiness(nullable:false)
     }
 }
