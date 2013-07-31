416e60fc920c98f3006ca55408c01fbd9b41054a
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-07-31 15:31:21 -0400

    860781-54242792 Fix the UserProfileEmailAddressControllerTests - they are currently failing.
    added a helper method for the LeadLineOfBusiness validation on 180 days expiration date for adding a  LeadLineOfBusiness with the same LineOfBusiness as one that is already associated with the Lead.

--- grails-app/services/com/cogda/multitenant/LeadService.groovy
+++ grails-app/services/com/cogda/multitenant/LeadService.groovy
@@ -1,5 +1,7 @@
 package com.cogda.multitenant
 
+import com.cogda.domain.admin.LineOfBusiness
+
 /**
  * LeadService
  * A service class encapsulates the core business logic of a Grails application
@@ -18,5 +20,42 @@ class LeadService {
 
     def getLead(id) {
         return Lead.get(id);
+    }
+
+    /**
+     * Retrieves a List of LeadLineOfBusiness objects that match
+     * the passed in LineOfBusiness.
+     * @param id  the Lead.id
+     * @param lineOfBusiness  a LineOfBusiness object
+     * @return List - a set of LeadLineOfBusiness objects if found
+     */
+    def findLeadLineOfBusinessesByLob(id, LineOfBusiness lineOfBusiness){
+        Lead lead = Lead.findById(id, [fetch:[linesOfBusiness:"eager"]])
+        def leadLinesOfBusinessWithMatchingLob = new HashSet()
+        if(lead){
+            leadLinesOfBusinessWithMatchingLob = lead.linesOfBusiness.findAll { LeadLineOfBusiness leadLineOfBusiness ->
+                    leadLineOfBusiness.lineOfBusiness == lineOfBusiness
+                }
+        }
+        return leadLinesOfBusinessWithMatchingLob
+    }
+
+    /**
+     * Retrieves a List of LeadLineOfBusiness objects that match
+     * the passed in LineOfBusiness and have an LeadLineOfBusiness.expirationDate that is less than the
+     * passed in expiration date.
+     * @param id  the Lead.id
+     * @param lineOfBusiness a LineOfBusiness object
+     * @param expirationDate - a set of LeadLineOfBusiness objects if found
+     */
+    def findLeadLineOfBusinessesByLobAndExpirationDate(id, LineOfBusiness lineOfBusiness, Date expirationDate){
+        def leadLinesOfBusinessWithMatchingLob = findLeadLineOfBusinessesByLob(id, lineOfBusiness)
+        def dateConditionMatches = new HashSet()
+        if(leadLinesOfBusinessWithMatchingLob){
+            dateConditionMatches = leadLinesOfBusinessWithMatchingLob.findAll { LeadLineOfBusiness leadLineOfBusiness ->
+                    leadLineOfBusiness.expirationDate <= expirationDate
+               }
+        }
+        return dateConditionMatches
     }
 }
