eaaae5807ba99596b47da858c7eb9b68b938e9cc
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-08-01 01:48:54 -0400

    860781-53566315 Our users should be able to see the submission builder as we build it.

---
+++ test/unit/com/cogda/domain/SubmissionControllerSpec.groovy
@@ -0,0 +1,16 @@
+package com.cogda.domain
+
+
+
+import grails.test.mixin.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(SubmissionController)
+class SubmissionControllerSpec {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
