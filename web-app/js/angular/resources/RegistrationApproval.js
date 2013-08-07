194de59528c830a5032b03b3cc92587ab1cd15dc
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-08-07 11:53:46 -0400

    860781-54532930 Create an administration portal native in the CogdaTwo system that can be accessed via cogdasolutions.cogdatwo.com.

--- web-app/js/angular/resources/RegistrationApproval.js
+++ web-app/js/angular/resources/RegistrationApproval.js
@@ -1,3 +1,17 @@
-angular.module('resources.registrationApproval', ['resources.restApi']).factory('RegistrationApproval', ['RestApi', function(RestApi) {
-    return RestApi.getRest('/registrationApproval/');
-}]);
+angular.module('resources.registrationApproval', ['ngResource']).factory('RegistrationApproval', function($resource) {
+    var restApi = {
+        getRest:getRest
+    }
+
+    function getRest(baseUrl){
+        return $resource(baseUrl + ':action/:id', {id: '@id'},   {
+            list:     {method: 'GET',  params:  {action: 'list'}, isArray: true},       // list objects
+            get:      {method: 'GET',  params:  {action: 'get'}     },                  // get one object
+            delete:   {method: 'POST', params:  {action: 'delete'}  },                  // delete an object
+            approve:  {method: 'POST', params:  {action:  'approve'} },
+            reject:   {method: 'POST', params:  {action:  'reject'}  }
+        });
+    }
+
+    return getRest('/registrationApproval/');
+});
