3dc4f8e52d0a5e775874247110d9b686d6666b91
Christopher Kwiatkowski <chris@rapidcommittee.com>
2013-08-08 07:35:58 -0400

    860781-54532930 Create an administration portal native in the CogdaTwo system that can be accessed via cogdasolutions.cogdatwo.com.
    committing some failing tests for the search method in the RegistrationService - need to release the code so that QA can begin to approve new CustomerAccounts via the Registration Approval admin pages.

--- grails-app/services/com/cogda/domain/onboarding/RegistrationService.groovy
+++ grails-app/services/com/cogda/domain/onboarding/RegistrationService.groovy
@@ -2,6 +2,7 @@ package com.cogda.domain.onboarding
 
 import com.cogda.common.RegistrationStatus
 import com.cogda.multitenant.CustomerAccountService
+import org.grails.datastore.mapping.query.api.Criteria
 
 /**
  * RegistrationService
@@ -33,5 +34,41 @@ class RegistrationService {
             // Approval Triggers the onboardCustomerAccount method
             customerAccountService.onboardCustomerAccount(registration)
         }
+    }
+
+    /**
+     * Registration search method
+     * ATTENTION: Testing is ongoing
+     * TODO: Complete testing of the search method and refine this method to search for RegistrationStatus as well.
+     */
+    public List search(params){
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)  // always pass in max so you get a PagedResultSet returned.
+        params.offset = params.offset ? params.int('offset') : 0
+        params.sort = params.sort ?: 'dateCreated'
+        params.order  = params.order  ?: 'desc'
+
+        String q = params.q?.trim()
+        if(!q){
+            return Registration.list(params)
+        }else{
+            q += "%"
+        }
+
+        def c = Registration.createCriteria()
+        List registrations = c.list(params.max, params.offset){
+            or {
+                ilike("firstName", q)
+                ilike("lastName", q)
+                ilike("username", q)
+                ilike("emailAddress", q)
+                ilike("companyName", q)
+                companyType {
+                    ilike("code", q)
+                }
+            }
+            order(params.sort, params.order)
+        }
+
+        return registrations
     }
 }
